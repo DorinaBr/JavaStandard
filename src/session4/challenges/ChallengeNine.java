3268efcaa30cfb7122093b3281b6faa0b1803787
Dorina <dorina.brahasteanu@gmail.com>
2023-09-13 14:39:26 +0300

    Added ten classes in package challenge on session4 branch

---
+++ src/session4/challenges/ChallengeNine.java
@@ -0,0 +1,18 @@
+package session4.challenges;
+
+//Write a Java program to create two String variables, countryOne with the value "USA" and countryTwo with the value "USA".
+// Check if countryOne and countryTwo are equal using the equals() method and print the result.
+// Then change the value of countryTwo to "UK" and check for equality again. Print the result.
+public class ChallengeNine {
+    public static void main(String[] args) {
+//        String countryOne = "USA";
+//        String countryTwo = "USA";
+//
+//        System.out.println(countryOne.equals(countryTwo)); // outputs true
+
+        String countryOne = "USA";
+        String countryTwo = "UK";
+
+        System.out.println(countryOne.equals(countryTwo)); // outputs false
+    }
+}
---
+++ src/session4/challenges/ChallengeSix.java
@@ -0,0 +1,22 @@
+package session4.challenges;
+
+import java.util.Scanner;
+
+//Write a Java program where you declare an integer variable temperature. Assign a value to it and print "Hot" if the temperature is more than 30,
+// "Warm" if the temperature is between 20 and 30, and "Cold" if it's less than 20.
+public class ChallengeSix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the temperature: ");
+        int temperature = scanner.nextInt();
+
+        if (temperature > 30) {
+            System.out.println("Hot");
+        } else if (temperature >= 20) {
+            System.out.println("Warm");
+        } else {
+            System.out.println("Cold");
+        }
+    }
+}
