9710d7f952ca975282be14bf9995711c17ec3e0e
Dorina <dorina.brahasteanu@gmail.com>
2023-10-03 21:08:57 +0300

    new branch

---
+++ src/session10/practice/bank_account/BankAccount.java
@@ -0,0 +1,28 @@
+package session10.practice.bank_account;
+
+public class BankAccount {
+
+    private double balance;
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("The amount is invalid");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance && amount > 0) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        }else{
+            System.out.println("The amount is invalid");
+        }
+    }
+
+    public double checkBalance(){
+        return balance;
+    }
+}
---
+++ src/session10/practice/inheritance/JavaDiamondProblem.java
@@ -0,0 +1,20 @@
+package session10.practice.inheritance;
+
+public class JavaDiamondProblem {
+}
+
+class Shape {
+
+}
+
+class Rectangle extends Shape{
+    void setArea(){}
+}
+
+class Square extends Shape{
+    void setArea(){}
+}
+
+//class Parallelogram extends Square, Rectangle {
+//
+//}
