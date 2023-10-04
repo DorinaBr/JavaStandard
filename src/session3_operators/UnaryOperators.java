b8f8e95d441794a382e187a419055f9ee14aed34
Dorina <dorina.brahasteanu@gmail.com>
2023-10-04 10:46:55 +0300

    added pckg3

---
+++ src/session3_operators/UnaryOperators.java
@@ -0,0 +1,12 @@
+package session3_operators;
+
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+        int firstNumber = +10;
+        int secondNumber = -20;
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+    }
+}
