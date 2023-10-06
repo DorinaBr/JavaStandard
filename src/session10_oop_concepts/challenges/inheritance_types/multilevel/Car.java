3b845440b03fb3638e7098e78337c845f936e9c9
Dorina <dorina.brahasteanu@gmail.com>
2023-10-06 10:38:39 +0300

    updated

--- src/session10_oop_concepts/challenges/inheritance_types/multilevel/Car.java
+++ src/session10_oop_concepts/challenges/inheritance_types/multilevel/Car.java
@@ -3,11 +3,13 @@ package session10_oop_concepts.challenges.inheritance_types.multilevel;
 public class Car {
 
     private String model;
+    private String make;
     private int year;
 
-    public Car(String model, int year) {
+    public Car(String model, String make, int year) {
         this.model = model;
         this.year = year;
+        this.make = make;
     }
 
     public void start() {
@@ -20,5 +22,17 @@ public class Car {
 
     public void accelerate() {
         System.out.println("The car accelerates.");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
