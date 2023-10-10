fcfdfdcb91f68838aea12378dedcd1090067fa5f
Dorina <dorina.brahasteanu@gmail.com>
2023-10-10 21:09:23 +0300

    added a new branch

---
+++ src/session12_polymorphism/practice/fitness_center/models/User.java
@@ -0,0 +1,9 @@
+package session12_polymorphism.practice.fitness_center.models;
+
+import java.util.UUID;
+
+public class User {
+
+    private UUID userId;
+    private PersonalInfo personalInfo;
+}
