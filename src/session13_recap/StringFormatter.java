b84ca6ba1a9a7041da38c7b14eda5c1487a8c18d
Dorina <dorina.brahasteanu@gmail.com>
2023-10-13 20:32:05 +0300

    added challenges on branch12

---
+++ src/session13_recap/StringFormatter.java
@@ -0,0 +1,19 @@
+package session13_recap;
+
+import java.util.Scanner;
+
+public class StringFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+
+        String result = String.format("%s is %d years old", name, age);
+        System.out.println(result);
+    }
+}
